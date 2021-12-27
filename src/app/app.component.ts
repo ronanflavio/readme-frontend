e54bdeebea9f46d5053831b2c10e818cfab58046
Ronan Flavio <ronan.flavio@hotmail.com>
2021-12-27 11:01:52 -0300

    hiding header based on url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,14 +8,42 @@ import { MatSidenav } from '@angular/material/sidenav';
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  @ViewChild('sidenav') sidenav: MatSidenav;
+  @ViewChild('sidenav') sidenav!: MatSidenav;
 
   public title = 'Read.me';
+  public showNavbar: boolean = true;
+  public showCreatePostButton: boolean = true;
+
+  private _hideNavbarWhen: string[] = [
+    '/post/create',
+  ];
+
+  private _hideCreatePostButtonWhen: string[] = [
+    '/post/create',
+  ];
+
+  constructor(
+    private _router: Router
+  ) {
+  }
+
+  ngOnInit(): void {
+      this._subscribeRouter();
+  }
 
   public close() {
     this.sidenav.close();
+  }
+
+  private _subscribeRouter(): void {
+    this._router.events.subscribe((value) => {
+      if (value instanceof NavigationEnd) {
+        this.showNavbar = !this._hideNavbarWhen.includes(value.url);
+        this.showCreatePostButton = !this._hideCreatePostButtonWhen.includes(value.url);
+      }
+    })
   }
 
 }
