21021c2881d2f3a455a4d1488f2dcc32f19f4c05
Ronan Flavio <ronan.flavio@hotmail.com>
2021-12-29 22:58:14 -0300

    using library to auto detect background color based on image

--- src/app/books/book-details/book-details.component.ts
+++ src/app/books/book-details/book-details.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import FastAverageColor from 'fast-average-color';
 
 @Component({
   selector: 'app-book-details',
   templateUrl: './book-details.component.html',
   styleUrls: ['./book-details.component.scss']
 })
-export class BookDetailsComponent implements OnInit {
+export class BookDetailsComponent implements AfterViewInit {
+
+  @ViewChild('header') header!: ElementRef;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+      this._setCoverBackground();
+  }
+
+  private _setCoverBackground(): void {
+    if (this.header) {
+      const fac = new FastAverageColor();
+      const cover = this.header.nativeElement.querySelector('.readme-book-cover');
+      const image = this.header.nativeElement.querySelector('img');
+      fac.getColorAsync(image)
+        .then(color => {
+          cover.style.backgroundColor = color.rgba;
+          cover.style.color = color.isDark ? '#fff' : '#000';
+        })
+    }
   }
 
 }
