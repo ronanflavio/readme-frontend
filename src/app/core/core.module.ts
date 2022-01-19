198cc72b986779f35bebcf83144a068e6de5f5b0
Ronan Flavio <ronan.flavio@hotmail.com>
2022-01-18 21:24:09 -0300

    moving components to inside its folder

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './components/header/header.component';
 import { MaterialModule } from '../material/material.module';
-import { FooterComponent } from './footer/footer.component';
-import { SideMenuComponent } from './side-menu/side-menu.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
 import { ElapsedTimePipe } from './pipes/elapsed-time.pipe';
 import { RouterModule } from '@angular/router';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
