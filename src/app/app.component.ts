0ef830eb69012feaddc9103d20a6ef3c9542dec1
Ronan Flavio <ronan.flavio@hotmail.com>
2022-05-12 22:36:55 -0300

    including download button for pwa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
 import { NavigationEnd, Router } from '@angular/router';
 import { OnlineStatusService, OnlineStatusType } from 'ngx-online-status';
@@ -13,12 +13,21 @@ export class AppComponent implements OnInit {
 
   @ViewChild('sidenav') sidenav!: MatSidenav;
 
+  @HostListener('window:beforeinstallprompt', ['$event'])
+  onbeforeinstallprompt(e) {
+    e.preventDefault();
+    this.deferredPrompt = e;
+    this.showInstallButton = true;
+  }
+
   public title = 'Read.me';
   public showNavbar: boolean = true;
   public showFooter: boolean = true;
   public isOnline: boolean = true;
   public onlineStatusCheck = OnlineStatusType;
   public status = OnlineStatusType.ONLINE
+  public deferredPrompt: any;
+  public showInstallButton = false;
 
   private _showNavbarWhen: string[] = [
     '/feed',
@@ -45,6 +54,20 @@ export class AppComponent implements OnInit {
 
   public close() {
     this.sidenav.close();
+  }
+
+  public addPwaToHomeScreen(): void {
+    this.showInstallButton = false;
+    this.deferredPrompt.prompt();
+    this.deferredPrompt.userChoice
+      .then((choiceResult: any) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        this.deferredPrompt = null;
+      });
   }
 
   private _subscribeRouter(): void {
